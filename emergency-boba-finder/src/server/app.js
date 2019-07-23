f1546f8a9070962434d54f02f3c2182887f81325
Shuotian Chen <shuoc@microsoft.com>
2019-07-23 15:03:30 -0700

    Remove commented out code

--- emergency-boba-finder/src/server/app.js
+++ emergency-boba-finder/src/server/app.js
@@ -2,28 +2,6 @@ const http = require('http');
 const mssql = require('../db/mssql')
 const express = require('express')
 
-// const hostname = '127.0.0.1';
-// const port = 3000;
-
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello World\n');
-// });
-
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-//   function done(interests) {
-//     console.log(interests);
-//   }
-//   // db.getInterests(done);
-//   //db.createUser({"FirstName": "Derp", "LastName": "Herp", "Location": "Derpville", "Distance": 0});
-//   var interests = mssql.getInterests(done);
-//   mssql.createUser({"FirstName": "Derp", "LastName": "Herp", "Location": "Derpville", "Distance": 0});
-//   console.log(interests)
-// });
-
-
 const app = express()
 const port = 3001
 
