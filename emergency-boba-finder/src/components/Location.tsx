954f55ef05e3f8f1dbddc0b870e6fcbf4c182003
Tabitha Hsia <tahsia@microsoft.com>
2019-07-22 17:23:17 -0700

    added axios

--- emergency-boba-finder/src/components/Location.tsx
+++ emergency-boba-finder/src/components/Location.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, Form } from 'semantic-ui-react';
+import axios from 'axios';
 
 export interface ILocation {
     location: string,
@@ -13,7 +14,20 @@ class Location extends React.Component<{}, ILocation>{
         this.state = {
             location: "",
         }
+        this.updateAddress = this.updateAddress.bind(this);
     }
+
+    updateAddress(e: any) {
+        e.preventDefault();
+        axios.post("/location")
+            .then(function(response){
+                console.log("response", response);
+            })
+            .catch(function(error){
+                console.log("error", error);
+            })
+    };
+
     render() {
         return (
             <div>
@@ -27,7 +41,7 @@ class Location extends React.Component<{}, ILocation>{
                         />
                     </Form.Field>
                 </Form>
-                <Button>Continue</Button>
+                <Button onClick={this.updateAddress}>Continue</Button>
             </div>
         );
     }
