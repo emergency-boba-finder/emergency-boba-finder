e8ace04d59c84279c6046c242b8b99ed96d1cb24
Meghan Reid <mereid@microsoft.com>
2019-07-23 18:48:35 -0700

    Adding routing and user home page

--- emergency-boba-finder/src/components/EmergencyBobaFinder.tsx
+++ emergency-boba-finder/src/components/EmergencyBobaFinder.tsx
@@ -1,14 +1,24 @@
-// import UserInformation from './UserInformation';
+import UserInformation from './UserInformation';
 import './EmergencyBobaFinder.css';
 
 import React from 'react';
+import UserHomePage from './UserHomePage';
+import { Route } from 'react-router';
+
 
 class EmergencyBobaFinder extends React.Component {
     render() {
         return (
             <div className="page">
-                This is our EmergencyBobaFinder website!
-                {/* <UserInformation userFullName={"Tabitha"} /> */}
+                <Route
+                    path='/'
+                    render={() => <UserInformation userFullName={"Tabitha"} />}
+                />
+                <Route
+                    path='/updateSettings'
+                    render={() => <UserInformation />}
+                />
+                <Route path="/homepage" component={UserHomePage} />
             </div>
         );
     }
